80373cea47b05dde3d00d1ffebda6a5ebff8347d
David Aramant <davidaramant@gmail.com>
2026-08-30 14:21:40 -0400

    Added script framework and some targets

---
+++ scripts/Program.cs
@@ -0,0 +1,20 @@
+﻿using CliWrap;
+using Scripts;
+using static Bullseye.Targets;
+
+Target("format", FormatAsync);
+Target("clean", CleanAsync);
+Target("build", BuildSolutionAsync);
+
+await RunTargetsAndExitAsync(args);
+return;
+
+static Task FormatAsync() =>
+	Cli.Wrap("dotnet").WithArguments("csharpier format .").WithWorkingDirectory(RepoPath.Root).ExecuteAsync();
+
+static Task CleanAsync() => Cli.Wrap("dotnet").WithArguments(c => c.Add("clean").Add(RepoPath.Solution)).ExecuteAsync();
+
+static Task BuildSolutionAsync() =>
+	Cli.Wrap("dotnet")
+		.WithArguments(c => c.Add("build").Add(RepoPath.Solution).Add("-c").Add("Release"))
+		.ExecuteAsync();
---
+++ scripts/RepoPath.cs
@@ -0,0 +1,30 @@
+namespace Scripts;
+
+public static class RepoPath
+{
+	static RepoPath()
+	{
+		Root = FindRoot();
+
+		static string FindRoot()
+		{
+			var dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+			do
+			{
+				if (File.Exists(Path.Combine(dir.FullName, "run.sh")))
+				{
+					return dir.FullName;
+				}
+
+				dir = dir.Parent;
+			} while (dir is not null);
+
+			throw new InvalidOperationException("Could not find root directory");
+		}
+	}
+
+	public static string Root { get; }
+
+	public static string Source => Path.Combine(Root, "src");
+	public static string Solution => Path.Combine(Source, "CompleteNatGeo.slnx");
+}
