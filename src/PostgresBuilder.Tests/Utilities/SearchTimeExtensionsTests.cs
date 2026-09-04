9cc56bc72e62d793fa1e08c5fc979656aa838234
David Aramant <davidaramant@gmail.com>
2026-09-04 16:03:32 -0400

    SearchTime conversion

---
+++ src/PostgresBuilder.Tests/Utilities/SearchTimeExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿using CompleteNatGeo.PostgresBuilder.Utilities;
+using Shouldly;
+
+namespace CompleteNatGeo.PostgresBuilder.Tests.Utilities;
+
+public class SearchTimeExtensionsTests
+{
+	[Theory]
+	[InlineData(19700101, 1970, 1, 1)]
+	[InlineData(19230928, 1923, 9, 28)]
+	public void ShouldConvertSearchTimeToDateOnly(int searchTime, int year, int month, int day)
+	{
+		searchTime.ToDate().ShouldBe(new DateOnly(year, month, day));
+	}
+}
---
+++ src/PostgresBuilder/Utilities/SearchTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace CompleteNatGeo.PostgresBuilder.Utilities;
+
+public static class SearchTimeExtensions
+{
+	public static DateOnly ToDate(this int searchTime)
+	{
+		var year = searchTime / 10000;
+		var month = (searchTime / 100) % 100;
+		var day = searchTime % 100;
+		return new DateOnly(year, month, day);
+	}
+}
